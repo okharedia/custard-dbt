f58b8c90b3375b54c3ea607950610a467d31404d
Joseph Okharedia <joseph@okharedia.com>
2025-10-26 02:20:52 +0100

    Add event handling functionality to silver_daily_schedule_dates.py, including a new Event model and a method to retrieve events from iCal strings within a specified date range.

--- models/silver/silver_daily_schedule_dates.py
+++ models/silver/silver_daily_schedule_dates.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from datetime import timedelta
-from utils.get_events_between import get_events_between, Event
+from datetime import datetime, timedelta
+from typing import List
+from icalendar import Calendar
+from recurring_ical_events import of as recurring_ical_events_of
+from pydantic import BaseModel
 
 def model(dbt, session):
     dbt.config(
@@ -54,3 +58,31 @@ def model(dbt, session):
     result_df.sort_values(by=["schedule_id", "event_date"], inplace=True)
 
     return result_df
+
+class Event(BaseModel):
+    start: datetime
+    end: datetime
+
+def get_events_between(
+    ical_string: str, range_start: datetime = None, range_end: datetime = None
+) -> List[Event]:
+    """
+    Get all events from an iCal string that occur between a start and end date.
+    If start_date or end_date are not provided, they default to 10 years in the past
+    and 10 years in the future, respectively.
+    """
+    calendar = Calendar.from_ical(ical_string)
+
+    if range_start is None:
+        range_start = datetime.now() - timedelta(days=365 * 10)
+
+    if range_end is None:
+        range_end = datetime.now() + timedelta(days=365 * 10)
+
+    events = recurring_ical_events_of(calendar).between(range_start, range_end)
+    date_ranges = []
+
+    for event in events:
+        date_ranges.append(Event(start=event.start, end=event.end))
+
+    return date_ranges
